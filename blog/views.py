6b36daaeca5a4af2c1b7ac62c3de39d07cfc1aeb
= <rat00@iesemilidarder.com>
2023-02-25 11:56:06 +0100

    Cambie el HTML para la página.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    # En linux la barra es al reves!!!
+    return render(request, 'blog\post_list.html', {})
