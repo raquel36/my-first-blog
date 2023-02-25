94a236bf9adc5b23f689202f28cbafab140944d7
= <rat00@iesemilidarder.com>
2023-02-25 13:45:28 +0100

    cambios para que cargue correctamente

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, get_object_or_404
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # En linux la barra es al reves!!!
-    return render(request, 'blog\post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
